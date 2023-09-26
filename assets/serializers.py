95d1eb3f7231466f53d14a5f0f5328ec033fade3
fabi8bit <fabi8bit@gmail.com>
2023-09-26 11:20:47 +0000

    Add is_checked field to assets/serializers.py

--- assets/serializers.py
+++ assets/serializers.py
@@ -1,21 +1,35 @@
 from rest_framework import serializers
 from .models import Asset, Project
+from checks.models import Check
 
 
 class AssetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    is_checked = serializers.SerializerMethodField()
     
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
-        
+
+    def get_is_checked(self, obj):
+        user = self.context['request'].user
+        project_id = obj.project_id.id
+        project = Project.objects.get(pk=project_id)
+        if project.owner == user:
+            check = Check.objects.filter(
+                owner=user,
+                asset_id=obj.id
+            ).first()
+            return check.id if check else None
+        return None
+
         
 
     class Meta:
         model = Asset
         fields = [
             'id', 'owner', 'asset_name', 'category', 'description',
-            'image', 'project_id', 'is_owner'
+            'image', 'project_id', 'is_owner', 'is_checked'
         ]
