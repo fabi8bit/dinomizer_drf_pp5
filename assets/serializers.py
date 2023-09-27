9f54b13da3477a5eef894cbf06240daad6bb4d03
fabi8bit <fabi8bit@gmail.com>
2023-09-27 10:02:49 +0000

    Add naturaltime to assets list on created_at and updated_at fields in assets/serializers.py

--- assets/serializers.py
+++ assets/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
 from .models import Asset
 from checks.models import Check
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 
 class AssetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     check_id = serializers.SerializerMethodField()
-    
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.created_at)
+    
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -27,5 +35,5 @@ class AssetSerializer(serializers.ModelSerializer):
         model = Asset
         fields = [
             'id', 'owner', 'asset_name', 'category', 'description',
-            'image', 'project_id', 'is_owner', 'check_id'
+            'image', 'created_at', 'updated_at', 'project_id', 'is_owner', 'check_id'
         ]
