e461cd03f5a271c38d7d966b1db16fd9a27e9350
fabi8bit <fabi8bit@gmail.com>
2023-09-27 09:06:58 +0000

    Add default pagination to list classes in settings.py
    Refactor list view for projects using generic views

--- dm_drf_api/settings.py
+++ dm_drf_api/settings.py
@@ -160,8 +160,17 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE':10,
+        'DATETIME_FORMAT': '%d %b %Y',
 }
+
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK ['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True #To enable token authentication
 JWT_AUTH_SECURE = True #To make sure they’re sent over HTTPS only
