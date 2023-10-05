c32a13a1824267d1c18c23933c84d9e78f12bfc1
fabi8bit <fabi8bit@gmail.com>
2023-10-05 13:26:52 +0000

    Fix profile model updated_at field

--- profiles/models.py
+++ profiles/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     role = models.CharField(max_length=32, blank=True)
