2e31669063468d5e4191c9b5051285f71aa79ea1
fabi8bit <fabi8bit@gmail.com>
2023-10-21 17:06:29 +0000

    Add new host string to allowed_hosts in settings.py

--- dm_drf_api/settings.py
+++ dm_drf_api/settings.py
@@ -38,6 +38,7 @@ DEBUG = 'DEBUG' in os.environ
 ALLOWED_HOSTS = [
     '8000-fabi8bit-dinomizerdrfpp-lj287mt52d5.ws-eu104.gitpod.io',
     '8000-fabi8bit-dinomizerdrfpp-lj287mt52d5.ws-eu105.gitpod.io',
+    '3000-fabi8bit-dinomizerreact-urps9b7jf4q.ws-eu105.gitpod.io/',
     os.environ.get('ALLOWED_HOST'),
     ]
 
@@ -218,3 +219,4 @@ JWT_AUTH_SAMESITE = 'None'
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'dm_drf_api.serializers.CurrentUserSerializer' 
 }
+
